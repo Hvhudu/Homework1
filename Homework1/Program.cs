dd1c507b549e5ea177a24b3e16c63658868b7847
Hvhudu <shtormbreker2@gmail.com>
2022-01-03 14:13:45 +0300

    Money

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -8,96 +8,79 @@ namespace Homework1
 {
     internal class Program
     {
-        class Auto
+        class Money
         {
-            private string name;
-            private int price;
-            private int volEngine;
-            private string typeTrans;
-            private int MaxSpeed;
-            private double Acceleration_to_100;
+            private int Grivn;
+            private int Cop;
 
-            public Auto()
+            public Money()
             {
-                name = " ";
-                price = 0;
-                volEngine = 0;
-                typeTrans = " ";
-                MaxSpeed = 0;
-                Acceleration_to_100 = 0;
+                Console.Write("MConstructor");
+                Grivn = Cop = 0;
             }
-            public Auto(string name,int price, int volEngine, string typeTrans, int MaxSpeed, double Acceleration_to_100)
+            public Money(int grivn,int cop)
             {
-                this.name = name;
-                this.price = price;
-                this.volEngine = volEngine;
-                this.typeTrans = typeTrans;
-                this.MaxSpeed = MaxSpeed;
-                this.Acceleration_to_100 = Acceleration_to_100;
+                Grivn = grivn;
+                Cop = cop;
             }
-            public void Input_TX()
+            public double Norm()
             {
-                Console.WriteLine("Введите название авто: ");
-                name = Console.ReadLine();
-                Console.WriteLine("Введите цену: ");
-                price = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Введите объём двигателя: ");
-                volEngine = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Введите тип коробки передач: ");
-                typeTrans = Console.ReadLine();
-                Console.WriteLine("Введите максимальную скорость: ");
-                MaxSpeed= Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Введите время разгона до 100км/ч: ");
-                Acceleration_to_100 = Convert.ToDouble(Console.ReadLine());
-
+                return Math.Sqrt(Grivn * Grivn + Cop * Cop);
             }
-            public void Print_TX()
+            public static Money operator +(Money a, Money b)
             {
-                Console.WriteLine($"Название автомобиля: {name}");
-                Console.WriteLine($"Цена: {price} руб");
-                Console.WriteLine($"Объём двигателя: {volEngine} л");
-                Console.WriteLine($"Тип коробки передач: {typeTrans}");
-                Console.WriteLine($"Максимальная скорость: {MaxSpeed} км/ч");
-                Console.WriteLine($"Разгон до 100 км/ч: {Acceleration_to_100} с");
+                int res_grivn = a.Grivn + b.Grivn;
+                int res_cop = a.Cop + b.Cop;
+                return new Money(res_cop, res_grivn);
             }
-            // struct Engine
-            //{
-            //    private double volume ;
-            //    private string type;
-
-            //    public double Volume { get { return volume; } set { volume = value; } }
-            //    public string Type { get { return type; } set { type = value; } }
-
-            //    public Engine(double volume,string type)
-            //    {
-            //        this.volume = volume;
-            //        this.type = type;
-            //    }
-            //}
-            //struct Transmission
-            //{
-            //    string type;
-            //}
-            //struct Drive_unit
-            //{
-            //    string type;
-            //}
-            //struct Consumption
-            //{
-            //    string cons;
-            //}
-            //struct Acceleration_to_100
-            //{
-            //    double acceler;
-            //}
+            public static Money operator -(Money a, Money b)
+            {
+                int res_grivn = a.Grivn - b.Grivn;
+                int res_cop = a.Cop - b.Cop;
+                return new Money(res_cop, res_grivn);
+            }
+            public static Money operator *(Money a, int b)
+            {
+                int res_grivn = a.Grivn * b;
+                int res_cop = a.Cop * b;
+                return new Money(res_cop, res_grivn);
+            }
+            public static Money operator /(Money a, int b)
+            {
+                int res_grivn = a.Grivn / b;
+                int res_cop = a.Cop / b;
+                return new Money(res_cop, res_grivn);
+            }
+            public static Money operator ++(Money a)
+            {
+                int res = a.Cop + 1;
+                return new Money(a.Grivn, res);
+            }
+            public static Money operator --(Money a)
+            {
+                int res = a.Cop - 1;
+                return new Money(a.Grivn, res);
+            }
+            public static bool operator <(Money a, Money b)
+            {
+                return a.Norm() < b.Norm();
+            }
+            public static bool operator >(Money a, Money b)
+            {
+                return a.Norm() > b.Norm();
+            }
+            public static bool operator ==(Money a, Money b)
+            {
+                return a.Equals(b);
+            }
+            public static bool operator !=(Money a, Money b)
+            {
+                return !a.Equals(b);
+            }
         }
         static void Main(string[] args)
         {
-            Auto lycan = new Auto();
-            lycan.Input_TX();
-            lycan.Print_TX();
-            
-            
+
         }
     }
 }
