99ed22a17b486f794cf303e49ef97430802d5a34
Hvhudu <shtormbreker2@gmail.com>
2022-01-22 13:01:53 +0300

    /

---
+++ HW/BinaryTree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW
+{
+    internal class BinaryTree<N> where N : IComparable<N>
+    {
+        private Node<N> root = null;
+        public int count = 0;
+
+        public void Add(N value)
+        {
+            if (root == null)
+                root = new Node<N>(value);
+            else
+                root.Add(value);
+        }
+        public void Display()
+        {
+            if (root != null)
+                root.DisplayNode("");
+            else
+                Console.WriteLine("empty tree");
+        }
+        public void Search(int count)
+        {
+            for (count = 0; count < 0; count ++);
+        }
+    }
+}
---
+++ HW/Node.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW
+{
+    internal class Node<N> where N : IComparable<N>
+    {
+        public N val { get; set; }
+        public int Rcount = 0;
+
+        private Node<N> left = null;
+        private Node<N> right = null;
+
+        public Node()
+        {
+            val = default;
+        }
+        public Node(N value)
+        {
+            val = value;
+        }
+        public void Add(N value)
+        {
+            if(val.CompareTo(value)<0)
+            {
+                if (left == null)
+                {
+                    left = new Node<N>(value);
+                    Rcount++;
+                }
+                else
+                {
+                    left.Add(value);
+                    Rcount++;
+                }
+            }
+            else
+            {
+                if (right == null)
+                {
+                    right = new Node<N>(value);
+                    Rcount++;
+                }
+                else
+                {
+                    right.Add(value);
+                    Rcount++;
+                }
+            }
+        }
+        public void DisplayNode(string offset)
+        {
+            Console.Write(offset);
+            Console.WriteLine(ToString());
+            if (right != null)
+                right.DisplayNode(offset + '\t');
+            else
+                Console.WriteLine(offset + "(null)");
+            if (left != null)
+                left.DisplayNode(offset + '\t');
+            else
+                Console.WriteLine(offset + "(null)");
+            Console.WriteLine();
+
+        }
+        public override string ToString()
+        {
+            if (val != null)
+                return val.ToString();
+            return "null";
+        }
+    }
+}
